3d12bea2449b7b0a01a6ac9f573fd274f7990a14
jpchato <jpchato@gmail.com>
2021-04-28 23:10:20 -0700

    api demo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,43 @@
 
-const getPokemon = async () => {
-  try {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
-
-    const pokemon = response.data;
-
-    console.log(pokemon);
-
-    return pokemon;
-  } catch (errors) {
-    console.error(errors);
-  }
+// Prevent the page from reloading when clicking submit
+function init(){
+  var form = document.getElementById("myForm");
+  form.addEventListener("submit", process, false);
 };
+function process(e){
+  console.log("Yikes!");
+  e.preventDefault()
+  return false;
+};
+document.addEventListener("DOMContentLoaded", init, false);
 
-getPokemon()
+let whatUp = () => {
+  console.log('whatup')
+}
+
+
+let myJsFunction = async () => {
+  var text=document.getElementById('Pokemon').value.toLowerCase()
+  
+  console.log(text)
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/'+text);
     
-console.log('dab')
+    const pokemon = response.data;
+    
+    // console.log(pokemon);
+    let pokemonAbilities = pokemon.abilities
+    let pokemonSprites = pokemon.sprites.front_default
+    console.log(pokemonSprites)
+    // console.log(pokemonAbilities)
+    $('#pokemonDiv').append('<h4>' + text + '\'s Abilities' + '</h4>')
+    $('#pokemonDiv').append('<img src="' + pokemonSprites + '" height="64px" width="64px">')
+    for (i=0; i < pokemon.abilities.length; i++){
+      let ability = (pokemonAbilities[i].ability.name)
+      $('#pokemonDiv').append('<p>'+pokemonAbilities[i].ability.name+'<p>')
+    }
+    return pokemon;
+      } catch (errors) {
+        console.error(errors);
+      }
+}
